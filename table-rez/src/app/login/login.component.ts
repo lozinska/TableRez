851e9efba5fdd6cade27f23eb20c05a5fcd2dcf9
Krystyna Lopez <klopez8@myseneca.ca>
2020-06-30 00:36:19 -0400

    Fix #5: add logic with connection to mysql to login component

--- table-rez/src/app/login/login.component.ts
+++ table-rez/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import{ActivatedRoute,Router} from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {CustomerService} from '../services/customer.service';
 
 @Component({
   selector: 'app-login',
@@ -8,34 +10,32 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-form:FormGroup;
+loginForm:FormGroup;
 public loginInvalid: boolean;
   private formSubmitAttempt: boolean;
   private returnUrl: string;
+  user:any;
 
   constructor(
-    private fb: FormBuilder,
+    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
+    private http:HttpClient,
+    private customerService: CustomerService
   ) { }
 
-  async ngOnInit() {
-    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/login';
-
-    this.form = this.fb.group({
-      username: ['', Validators.email],
+   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.email],
       password: ['', Validators.required]
     });
+  //  this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/login';
   }
 
-  async onSubmit() {
-    this.loginInvalid = false;
-    this.formSubmitAttempt = false;
-    if (this.form.valid) {
-        const username = this.form.get('username').value;
-        const password = this.form.get('password').value;
-    } else {
-      this.formSubmitAttempt = true;
-    }
-  }
+ login(){
+
+this.customerService.loginUser(this.loginForm.get('email').value, this.loginForm.get('password').value)
+  .then(()=>this.router.navigate(['/']))
+
+};
 }
