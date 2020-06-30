3fd85ab9f0783037b3456e1ecb065a193f9bd867
Krystyna Lopez <klopez8@myseneca.ca>
2020-06-30 00:50:12 -0400

    Fix #6: adding missing server file

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const restaurants = require('./models/restaurants');
 const user = require ('./models/users');
+const login=require('./models/login');
 const mysql=require('mysql')
 const connection = mysql.createConnection({
  host     : 'mymysql.senecacollege.ca',
@@ -16,18 +17,11 @@ const port = process.env.PORT || 8080;
 
 const app = express()
 
-//app.use(express.static(path.join(__dirname,'table-rez')));
-/*app.use(cors());
-app.use(bodyParser.json());
-app.use('/register',userRoute);
-app.get('/',(req,res)=>{
- res.sendFile(__dirname+'/table-rez/src/'+'index.html')
-
-})*/
   .use(cors())
   .use(bodyParser.json())
   .use(restaurants(connection))
   .use(user(connection))
+  .use(login(connection))
 app.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 });
